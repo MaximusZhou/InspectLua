fd3101ad6b71da541a1a707bc069396b40cc3266
MaximusZhou <zhougang009@gmail.com>
2014-02-12 09:57:01 +0800

    a simple lua interpreter for study lua api

---
+++ simple_lua_interpreter.c
@@ -0,0 +1,39 @@
+/*
+	
+	Example:
+	$gcc -Wall simple_lua_interpreter.c -llua -lm -ldl -o simple_lua_interpreter
+
+	Analyse:
+	http://stackoverflow.com/questions/4743233/is-usr-local-lib-searched-for-shared-libraries: About /usr/local/lib
+	http://fangmenghu.blog.163.com/blog/static/124582020104171451565/: About /lib,/usr/lib and /usr/local/lib
+	https://erex.sinaapp.com/?p=126: About compile search path
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+#include "lua.h"
+#include "lauxlib.h"
+#include "lualib.h"
+
+int main(void)
+{
+	char buf[256];
+	int error;
+	lua_State *L = luaL_newstate();
+	luaL_openlibs(L);
+
+	while(fgets(buf,sizeof(buf),stdin) != NULL)
+	{
+		error = luaL_loadbuffer(L,buf,strlen(buf),"line") || lua_pcall(L,0,0,0);
+		
+		if(error)
+		{
+			fprintf(stderr,"%s",lua_tostring(L,-1));
+			lua_pop(L,1);
+		}
+	}
+	lua_close(L);
+
+	return 0;
+}
