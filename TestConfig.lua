4a8fbbadf6da92e14649d9563634d33ee25e6925
MaximusZhou <zhougang009@gmail.com>
2014-02-16 18:37:44 +0800

    add use table and lua function in c

--- TestConfig.lua
+++ TestConfig.lua
@@ -1,3 +1,12 @@
 --test confg
 width = 10
 height = 20
+
+--test table
+--background = "BLUE"
+background = {r = 0.3, g = 0.1, b = 0}
+
+--test lua function in c
+function f(x,y)
+	return (x^2 * math.sin(y))/(1 - x) --luaL_openlibs(L) must be called in c 
+end
